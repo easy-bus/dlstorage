a5e3b0fb678678231fb5f89788e72f41e66337f3
letsfire <249008728@qq.com>
2021-01-04 13:23:10 +0800

    add allow_retry

--- go.mod
+++ go.mod
@@ -3,18 +3,18 @@ module github.com/easy-bus/dlstorage
 go 1.12
 
 require (
-	github.com/easy-bus/bus v0.0.0-20200310024416-638b62107f74
-	github.com/go-pg/pg/v9 v9.1.5
-	github.com/go-pg/urlstruct v0.4.0 // indirect
-	github.com/golang/protobuf v1.3.5 // indirect
+	github.com/easy-bus/bus v0.0.0-20210104051000-c92e6c5c0edc
+	github.com/go-pg/pg/v9 v9.2.0
+	github.com/go-pg/urlstruct v1.0.0 // indirect
+	github.com/golang/protobuf v1.4.3 // indirect
 	github.com/kr/text v0.2.0 // indirect
-	github.com/letsfire/utils v0.0.0-20200331101103-1ea3f3e4b618
+	github.com/letsfire/utils v0.0.0-20201225030618-f0d02dcb15db
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
-	github.com/stretchr/objx v0.2.0 // indirect
-	github.com/vmihailenco/bufpool v0.1.11 // indirect
-	github.com/vmihailenco/msgpack/v4 v4.3.11 // indirect
-	golang.org/x/crypto v0.0.0-20200406173513-056763e48d71 // indirect
-	golang.org/x/net v0.0.0-20200324143707-d3edc9973b7e // indirect
+	github.com/segmentio/encoding v0.2.4 // indirect
+	github.com/vmihailenco/tagparser v0.1.2 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+	golang.org/x/net v0.0.0-20201224014010-6772e930b67b // indirect
+	google.golang.org/appengine v1.6.7 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
-	gopkg.in/yaml.v2 v2.2.8 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- pgsql.go
+++ pgsql.go
@@ -2,22 +2,50 @@ package dlstorage
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/easy-bus/bus"
 	"github.com/go-pg/pg/v9"
 	"github.com/letsfire/utils"
 )
 
+// pgsqlDLModel
+type pgsqlDLModel struct {
+	Id         string `pg:"id,use_zero" json:"id"`
+	Queue      string `pg:"queue,use_zero" json:"queue"`
+	Data       []byte `pg:"data,use_zero" json:"data"`
+	AllowRetry bool   `pg:"allow_retry,use_zero" json:"allow_retry"`
+	CreatedAt  int64  `pg:"created_at,use_zero" json:"created_at"`
+}
+
 type pgsqlDLStorage struct {
-	sql string
-	db  *pg.DB
+	db    *pg.DB
+	table string
 }
 
 func (sds *pgsqlDLStorage) Store(queue string, data []byte) error {
-	_, err := sds.db.Exec(sds.sql, utils.GenerateSeqId(), queue, string(data))
+	sql := fmt.Sprintf("INSERT INTO %s (id, queue, data, allow_retry, created_at) VALUES (?, ?, ?, ?, ?)", sds.table)
+	_, err := sds.db.Exec(sql, utils.GenerateSeqId(), queue, string(data), false, time.Now().Unix())
+	return err
+}
+
+func (sds *pgsqlDLStorage) Fetch(queue string) (map[string][]byte, error) {
+	var bs = make(map[string][]byte)
+	var ms = make([]pgsqlDLModel, 0)
+	sql := fmt.Sprintf("SELECT * FROM %s WHERE queue = ? AND allow_retry = ?", sds.table)
+	_, err := sds.db.Query(&ms, sql, queue, true)
+	for _, m := range ms {
+		bs[m.Id] = m.Data
+	}
+	return bs, err
+}
+
+func (sds *pgsqlDLStorage) Remove(id string) error {
+	sql := fmt.Sprintf("DELETE FROM %s WHERE id = ?", sds.table)
+	_, err := sds.db.Exec(sql, id)
 	return err
 }
 
 func NewPGSQL(table string, db *pg.DB) bus.DLStorageInterface {
-	return &pgsqlDLStorage{sql: fmt.Sprintf("INSERT INTO %s (id, queue, data) VALUES (?, ?, ?)", table), db: db}
+	return &pgsqlDLStorage{db: db, table: table}
 }
